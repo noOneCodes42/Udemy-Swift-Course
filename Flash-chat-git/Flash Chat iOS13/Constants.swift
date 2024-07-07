bc392be66839d0726602c2dcda5f3c24a0641177
noOneCodes42 <117050732+noOneCodes42@users.noreply.github.com>
2024-07-06 20:57:43 -0700

    Add files via upload

---
+++ Flash-chat-git/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Neel Arora on 6/19/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+struct Constants {
+    static let appName = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+        static let collectionName2 = "name"
+        static let nameField = "name2"
+    }
+}
---
+++ Flash-chat-git/Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Neel Arora on 6/19/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String // The User Email
+    let body: String
+}
